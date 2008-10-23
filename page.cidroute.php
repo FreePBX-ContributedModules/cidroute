64a0d42cac3665cc761cdb0aae06cc43e0a7cd27
Rob Thomas <xrobau@gmail.com>
2008-10-23 04:48:29 +0000

    It's looking less like random typing now..

--- page.cidroute.php
+++ page.cidroute.php
@@ -10,37 +10,40 @@
 //MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 //GNU General Public License for more details.
 
-isset($_REQUEST['action'])?$action = $_REQUEST['action']:$action='';
 
+$tabindex = 0;
+// What are we doing..
+isset($_REQUEST['action'])?$action = $_REQUEST['action']:$action='';
 //the item we are currently displaying
 isset($_REQUEST['itemid'])?$itemid=$_REQUEST['itemid']:$itemid='';
 
+// Where we are
 $dispnum = "cidroute"; //used for switch on config.php
 
 //if submitting form, update database
-	print_r($_REQUEST);
 if(isset($_POST['action'])) {
 	print "I have a post\n";
+	print_r($_POST);
 	switch ($action) {
 		case "add":
 			cidroute_add($_POST);
 			redirect_standard();
 		break;
 		case "delete":
-			cidroute_del($itemid);
-			needreload();
-			redirect_standard();
+	#		cidroute_del($itemid);
+	#		needreload();
+	#		redirect_standard();
 		break;
 		case "edit":
-			cidroute_edit($itemid,$_POST);
-			needreload();
-			redirect_standard('itemid');
+	#		cidroute_edit($itemid,$_POST);
+	#		needreload();
+	#		redirect_standard('itemid');
 		break;
 	}
 }
 
 //get list of callerid lookup sources
-$cidsources = cidroute_list();
+$cidmaps = cidroute_list();
 ?>
 
 </div> <!-- end content div so we can display rnav properly-->
@@ -49,10 +52,10 @@ $cidsources = cidroute_list();
 <div class="rnav"><ul>
     <li><a id="<?php echo ($itemid=='' ? 'current':'') ?>" href="config.php?display=<?php echo urlencode($dispnum)?>"><?php echo _("Add Route Map")?></a></li>
 <?php
-if (isset($cidsources)) {
-	foreach ($cidsources as $cidsource) {
-		if ($cidsource['cidroute_id'] != 0)
-			echo "<li><a id=\"".($itemid==$cidsource['cidroute_id'] ? 'current':'')."\" href=\"config.php?display=".urlencode($dispnum)."&itemid=".urlencode($cidsource['cidroute_id'])."\">{$cidsource['description']}</a></li>";
+if (isset($cidmaps)) {
+	foreach ($cidmaps as $cidsource) {
+		if ($cidsource['destid'] != 0)
+			echo "<li><a id=\"".($itemid==$cidsource['destid'] ? 'current':'')."\" href=\"config.php?display=$dispnum&action=edit&itemid=".$cidsource['destid']."\">{$cidsource['name']}</a></li>";
 	}
 }
 ?>
@@ -62,59 +65,27 @@ if (isset($cidsources)) {
 <?php
 if ($action == 'delete') {
 	echo '<br><h3>'._("Route map").' '.$itemid.' '._("deleted").'!</h3>';
-} else {
-	if ($itemid){ 
-		//get details for this source
-		$thisItem = cidroute_get($itemid);
+} elseif ($action == 'edit' ) {
+
+	if (isset($itemid)) { 
+		showEdit($itemid, $cidmaps);
 	} else {
-		$thisItem = Array( 'description' => null, 'destid' => null);
+		showNew();
 	}
-
-	$delURL = $_SERVER['PHP_SELF'].'?'.$_SERVER['QUERY_STRING'].'&action=delete';
-	$delButton = "
-			<form name=delete action=\"{$_SERVER['PHP_SELF']}\" method=POST>
-				<input type=\"hidden\" name=\"display\" value=\"{$dispnum}\">
-				<input type=\"hidden\" name=\"itemid\" value=\"{$itemid}\">
-				<input type=\"hidden\" name=\"action\" value=\"delete\">
-				<input type=submit value=\""._("Delete Route Map")."\">
-			</form>";
-	
-?>
-
-	<h2><?php echo ($itemid ? _("Route:")." ". $itemid : _("Create Route")); ?></h2>
-
-
-
-<?
-#	<script type="text/javascript" src="modules/cidroute/js/autosuggest.js" charset="utf-8"></script>
-#
-#	<div>
-#	<link rel="stylesheet" href="modules/cidroute/js/autosuggest.css" type="text/css" media="screen" charset="utf-8" />
-#<form method="get" action="" class="asholder">
-#	<label for="region">Enter Area</label>
-#
-#	<input style="width: 200px" type="text" id="testinput" value="" /> 
-#	<input type="submit" value="submit" />
-#</form>
-#</div>
-#	<script type="text/javascript"> var options = { script: "modules/cidroute/js/ajax.php?type=global&limit=10&", 
-#			varname:"input",
-#			json: true, 
-#			shownowresults:true,
-#			maxresults:10 };
-#	var as = new bsn.AutoSuggest('region', options);
-#	</script>
-?>
+} else {
+	showNew();
+}
 
 
 	
+function showNew() {
+?>
 	<p style="width: 80%"><?php echo ($itemid ? '' : _("This is where you maintain your Caller Location Routing interface. ")); ?></p>
 	<p style="width: 80%"><?php echo ($itemid ? '' : _("Select a Name and Destination. You will be able to edit the mapping by clicking on the map name on the right.")); ?></p>
 
-        <form name="cidroute" action="<?php  $_SERVER['PHP_SELF'] ?>" method="post">
+<form name="cidroutemap" action="<? echo $_SERVER['PHP_SELF'] ?>" method="post">
         <input type="hidden" name="action" value="add">
-<input name="Submit" type="submit" value="<?php echo _("Go"); ?>" tabindex="<?php echo ++$tabindex;?>">
-</form>
+	<input type="hidden" name="display" value="<? echo $dispnum; ?>">
         <table>
         <tr><td colspan="2"><h5><?php  echo ($extdisplay ? _("Edit Route Destination") : _("Add Route Destination")) ?><hr></h5></td></tr>
         <tr>
@@ -127,10 +98,47 @@ if ($action == 'delete') {
 
 <?php
 //draw goto selects
-//echo drawselects($dest,0);
+echo drawselects($dest,0);
 ?>
 
+        <input name="Submit" type="submit" value="<?php echo _("Go"); ?>" tabindex="<?php echo ++$tabindex;?>">
+	</form>
 
 <?php		
-} //end if action == delete
+}
+
+function showEdit($itemid,$cidmaps) {
+//	$thisItem = cidroute_get($itemid);
+
+	$delURL = $_SERVER['PHP_SELF'].'?'.$_SERVER['QUERY_STRING'].'&action=delete';
+	$delButton = "
+<form name=delete action=\"{$_SERVER['PHP_SELF']}\" method=POST>
+	<input type=\"hidden\" name=\"display\" value=\"{$dispnum}\">
+	<input type=\"hidden\" name=\"itemid\" value=\"{$itemid}\">
+	<input type=\"hidden\" name=\"action\" value=\"delete\">
+	<input type=submit value=\""._("Delete Route Map")."\">
+</form>";
+	
+	print "<h2>"._("Route:")." ". $cidmaps[$itemid-1]['name']."</h2>";
+
+?>
+	<script type="text/javascript" src="modules/cidroute/js/autosuggest.js" charset="utf-8"></script>
+	<link rel="stylesheet" href="modules/cidroute/js/autosuggest.css" type="text/css" media="screen" charset="utf-8" />
+
+	<div>
+	<form method="get" action="" class="asholder">
+		<label for="region">Quick Selection:</label>
+		<input style="width: 200px" type="text" id="region" value="" /> 
+		<input type="submit" value="submit" />
+	</form>
+	</div>
+
+	<script type="text/javascript">
+ var options = { script: "modules/cidroute/js/ajax.php?type=global&limit=10&", 
+	varname:"input", json: true, shownowresults:true, maxresults:10 };
+ var as = new bsn.AutoSuggest('region', options);
+	</script>
+
+<?php
+} 
 ?>
